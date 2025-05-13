31b85adb1a2a53c8ebee7c8a20e46b900a0ea42e
Salvatore La Rosa <salvatorelarosapr@gmail.com>
2025-05-13 22:16:51 +0200

    Post credenziali per la registrazione

--- sessionManager/src/main/java/com/example/sessionmanager/controller/AppController.java
+++ sessionManager/src/main/java/com/example/sessionmanager/controller/AppController.java
@@ -1,13 +1,22 @@
 package com.example.sessionmanager.controller;
 
 
+import com.example.sessionmanager.dto.AuthDTO;
+import com.example.sessionmanager.model.Data;
+import com.example.sessionmanager.repository.DataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class AppController {
+
+    @Autowired
+    private DataRepository repo;
 
     @GetMapping("/login")
     public String login() {
@@ -25,22 +34,13 @@ public class AppController {
     @PostMapping("/register")
     public String register(@RequestParam String email, @RequestParam String password) {
         System.out.println("Registrazione utente: " + email + ", " + password);
-        // Qui puoi aggiungere salvataggio nel database, validazioni ecc.
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        Data data = new Data();
+        data.setEmail(email);
+        data.setPassword(encoder.encode(password));
+
+        repo.save(data);
         return "redirect:/login";
     }
-
-    /*
-
-    @Autowired
-    private DataRepository repo;
-
-
-    @PostMapping("/send")
-    public ResponseEntity<String> send(@RequestParam String content) {
-        repo.save(new Data(content));// Salva il dato in schema_a
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject("http://serviceb:8080/B/recive", content, String.class); //manda il dato a service_b notare url mappato interno
-        return ResponseEntity.ok("Saved in A and sended to B");
-    }
-    */
 }
