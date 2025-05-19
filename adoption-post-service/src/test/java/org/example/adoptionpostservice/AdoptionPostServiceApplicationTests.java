08708578d26e6812b5a86c159f4ef122c5bb5e15
StefanoAtzeni2001 <stefano.atzeni01@gmail.com>
2025-05-19 09:18:26 +0200

    initial config

---
+++ adoption-post-service/src/test/java/org/example/adoptionpostservice/AdoptionPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.adoptionpostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
