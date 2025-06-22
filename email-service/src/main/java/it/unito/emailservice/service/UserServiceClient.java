6c4c2687565229161d5698e77f5dba8491815b75
evafiori <evafiori@hotmail.it>
2025-06-22 19:05:08 +0200

    sistemata comunicazione email - user con variabile globale per url

--- email-service/src/main/java/it/unito/emailservice/service/UserServiceClient.java
+++ email-service/src/main/java/it/unito/emailservice/service/UserServiceClient.java
@@ -4,15 +4,17 @@ import it.unito.emailservice.dto.EmailRequestDto;
 import it.unito.emailservice.dto.EmailResponseDto;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 public class UserServiceClient {
 
     private final WebClient webClient;
 
-    public UserServiceClient(WebClient.Builder builder) {
+    public UserServiceClient(WebClient.Builder builder,
+                             @Value("${app.webclient.userservice}") String userServiceBaseUrl) {
         this.webClient = builder
-                .baseUrl("http://userservice:8080")
+                .baseUrl(userServiceBaseUrl) //"http://user-service:8083"
                 .build();
     }
 
